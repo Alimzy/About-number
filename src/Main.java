3bbb87a2547f8da881794a536191637b133ea386
Alimzy <salamialameen20@gmail.com>
2026-03-06 08:18:08 +0100

    my push

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 35;
+        int number = 5;
 
         if(isEven(number)) System.out.println("This number is an even number");
         if(isOdd(number)) System.out.println("This number is an odd number");
         if(isPrimeNumber(number)) System.out.println("This number is a prime number");
+        if(isCompositeNumber(number)) System.out.println("This number is a composite number");
         if(isSquare(number)) System.out.println("This number is a square number");
+        if(isCube(number)) System.out.println("This number is a cube number");
         if(isPerfectNumber(number)) System.out.println("This number is a perfect number");
-        System.out.println("The factorial of this number is " + factorial(number));
-
+        if(isAbundantNumber(number)) System.out.println("This number is an abundant number");
+        if(isDeficientNumber(number)) System.out.println("This number is a Deficient number");
+        if(isPositive(number)) System.out.println("This number is a positive number");
+        if(isNegative(number)) System.out.println("This number is a Negative number");
+        if(isPalindrome(number)) System.out.println("This number is a Palindrome number");
+        if(isBinary(number)) System.out.println("This number is a Binary number");
+        System.out.println(digitalSum(number));
+        System.out.println(factorialOf(number));
     }
+
+    public static boolean isPositive(int number){
+        return number > 0;
+    }
+
+    public static boolean isNegative(int number){
+        return number < 0;
+    }
+
+    public static boolean isZero(int number){
+        return number == 0;
+    }
+
 
     public static boolean isEven(int number) {
 
@@ -35,9 +56,33 @@ public class Main {
         return false;
     }
 
+    public static boolean isCompositeNumber(int number) {
+        int factors = 0;
+
+        for (int count = 1; count <= number; count++) {
+            if (number % count == 0) {
+                factors++;
+            }
+        }
+        if (factors <= 2) {
+
+            return false;
+        }
+        return true;
+    }
+
     public static boolean isSquare(int number) {
         for (int counter = 1; counter < number; counter++) {
             if (counter * counter == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isCube(int number) {
+        for (int counter = 1; counter < number; counter++) {
+            if (counter * counter * counter == number) {
                 return true;
             }
         }
@@ -54,15 +99,86 @@ public class Main {
         return sum == number;
     }
 
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversed = 0;
 
-    public static int  factorial(int number) {
-        int factorial = 1;
-
-        for (int count = 1; count < number; count++) {
-            factorial = factorial * count;
+        while (number > 0) {
+            reversed = reversed * 10 + (number % 10);
+            number /= 10;
         }
+
+        return originalNumber == reversed;
+    }
+
+    public static boolean isAbundantNumber(int number) {
+        int sum = 0;
+        for (int count = 1; count < number; count++) {
+            if (number % count == 0) {
+                sum += count;
+            }
+        }
+        return sum > number;
+    }
+
+    public static boolean isDeficientNumber(int number) {
+        int sum = 0;
+        for (int count = 1; count < number; count++) {
+            if (number % count == 0) {
+                sum += count;
+            }
+        }
+        return sum < number;
+    }
+
+
+    public static boolean isBinary(int number){
+        int lastDigit;
+        while(number > 0){
+             lastDigit = number % 10;
+            if(lastDigit != 0 && lastDigit != 1){
+                return false;
+            }
+            number /= 10;
+        }
+        return true;
+
+    }
+
+    public static boolean isArmStrongNumber(int number){
+        String digits = ""+ number;
+        int digit = number;
+        int mySum = 0;
+        int numberLength = digits.length();
+        while (digit!= 0){
+            int lastDigit = digit % 10;
+            int exponential = (int) Math.pow(lastDigit, numberLength);
+            mySum += exponential;
+            digit = digit / 10;
+        }
+        return mySum == number;
+    }
+
+    public static int digitalSum(int number){
+        int mySum = 0;
+        while (number > 0){
+            int lastDigit = number % 10;
+            mySum += lastDigit;
+            number /= 10;
+        }
+        return mySum;
+    }
+
+    public static int factorialOf(int number){
+        int factorial = 1;
+        for(int count =1; count <= number;count++){
+            factorial *= count;
+
+        }
+
         return factorial;
-}
+    }
+
 
 
 
